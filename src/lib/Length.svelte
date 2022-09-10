9dfa6995cc0ce6d0b05c39ab617cc10132ef34bb
Caleb Rivera <calebrossr@gmail.com>
2022-09-10 02:20:04 -0400

    Add UI components

---
+++ src/lib/Length.svelte
@@ -0,0 +1,4 @@
+<div class="absolute left-1 top-1 bg-gray-900 text-white px-6 py-3 rounded">
+  <div class="font-bold">Name</div>
+  <div class="font-bold">Length</div>
+</div>
---
+++ src/lib/UI.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Counter from "./Length.svelte";
+
+  import ScoreBoard from "./Leaderboard.svelte";
+</script>
+
+<ScoreBoard />
+<Counter />
+<slot />
