fd0e0d324000f0c2e118084c1297d0bdc9a572d4
Caleb Rivera <calebrossr@gmail.com>
2022-09-11 09:37:50 -0400

    Make leaderboard and length transparent

--- src/lib/Length.svelte
+++ src/lib/Length.svelte
@@ -1,4 +1,4 @@
-<div class="fixed left-4 top-4">
+<div class="fixed left-4 top-4 opacity-95">
   <div class="rounded bg-gray-900 px-6 py-3 text-white">
     <div class="font-bold">Name</div>
     <div class="font-bold">Length</div>
