5de3e3d201ef552ef27cbe17b783d8ea6e2f16b1
Caleb Rivera <calebrossr@gmail.com>
2022-09-11 10:12:13 -0400

    Change text size

--- src/lib/Length.svelte
+++ src/lib/Length.svelte
@@ -5,7 +5,7 @@
 
 <div class="fixed left-4 top-4 opacity-95">
   <div class="rounded bg-gray-900 px-6 py-3 text-white">
-    <div class="font-bold">Name: {playerName}</div>
-    <div class="font-bold">Length: {playerLength}</div>
+    <div class="text-xl font-bold">Name: {playerName}</div>
+    <div class="text-lg">Length: {playerLength}</div>
   </div>
 </div>
